d63e7104217059c4d797f3d1ba7ce7ceb3076e82
haim <haim@fhenix.io>
2025-04-07 19:12:11 +0300

    wording & future removal

--- sidebars.ts
+++ sidebars.ts
@@ -246,11 +246,6 @@ const sidebars: SidebarsConfig = {
     },
     {
       type: 'doc',
-      id: 'devdocs/future-plans',
-      label: 'Future Plans',
-    },
-    {
-      type: 'doc',
       id: 'devdocs/research-and-publications',
       label: 'Research in Fhenix',
     },
